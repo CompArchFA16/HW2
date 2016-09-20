a9935774bff241bdb34b541169eb76fccca4ddd4
David Zhu <hdavidzhu@gmail.com>
2016-09-20 16:25:23 -0400

    ADD: Tests for full adder.

--- adder.t.v
+++ adder.t.v
@@ -3,12 +3,28 @@
 `include "adder.v"
 
 module testFullAdder();
-    reg a, b, carryin;
-    wire sum, carryout;
+  reg a, b, carryin;
+  wire sum, carryout;
 
-    behavioralFullAdder adder (sum, carryout, a, b, carryin);
+  behavioralFullAdder dut(sum, carryout, a, b, carryin);
 
-    initial begin
-        // Your test code here
-    end
+  initial begin
+    $display("a b cIn | Sum cOut | expCOut expSum");
+    a = 0; b = 0; carryin = 0; #1000
+    $display("%b %b   %b |   %b    %b |       0      0", a, b, carryin, sum, carryout);
+    a = 0; b = 0; carryin = 1; #1000
+    $display("%b %b   %b |   %b    %b |       1      0", a, b, carryin, sum, carryout);
+    a = 0; b = 1; carryin = 0; #1000
+    $display("%b %b   %b |   %b    %b |       1      0", a, b, carryin, sum, carryout);
+    a = 0; b = 1; carryin = 1; #1000
+    $display("%b %b   %b |   %b    %b |       0      1", a, b, carryin, sum, carryout);
+    a = 1; b = 0; carryin = 0; #1000
+    $display("%b %b   %b |   %b    %b |       1      0", a, b, carryin, sum, carryout);
+    a = 1; b = 0; carryin = 1; #1000
+    $display("%b %b   %b |   %b    %b |       0      1", a, b, carryin, sum, carryout);
+    a = 1; b = 1; carryin = 0; #1000
+    $display("%b %b   %b |   %b    %b |       0      1", a, b, carryin, sum, carryout);
+    a = 1; b = 1; carryin = 1; #1000
+    $display("%b %b   %b |   %b    %b |       1      1", a, b, carryin, sum, carryout);
+  end
 endmodule
