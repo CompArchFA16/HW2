3758c9e3ef1fcdea0aab9c6936656cbd186a617f
tj-kim <taejin.kim96@gmail.com>
2016-09-21 18:41:50 -0400

    tjkim hw 2 submission

--- adder.t.v
+++ adder.t.v
@@ -6,9 +6,31 @@ module testFullAdder();
     reg a, b, carryin;
     wire sum, carryout;
 
-    behavioralFullAdder adder (sum, carryout, a, b, carryin);
+    //behavioralFullAdder adder (sum, carryout, a, b, carryin);
+    structuralFullAdder adder (sum, carryout, a, b, carryin);
 
     initial begin
+    // Dump trace to a file. Open with gtkwave.
+    $dumpfile("adder.vcd");
+    $dumpvars;
+
         // Your test code here
+    $display("A  B  C_in|  S  C_out");
+    a=0;b=0;carryin=0; #1000 
+    $display("%b  %b   %b  |  %b  %b", a, b, carryin, sum, carryout);
+    a=1;b=0;carryin=0; #1000 
+    $display("%b  %b   %b  |  %b  %b", a, b, carryin, sum, carryout);
+    a=0;b=1;carryin=0; #1000 
+    $display("%b  %b   %b  |  %b  %b", a, b, carryin, sum, carryout);
+    a=0;b=0;carryin=1; #1000 
+    $display("%b  %b   %b  |  %b  %b", a, b, carryin, sum, carryout);
+    a=1;b=1;carryin=0; #1000 
+    $display("%b  %b   %b  |  %b  %b", a, b, carryin, sum, carryout);
+    a=1;b=0;carryin=1; #1000 
+    $display("%b  %b   %b  |  %b  %b", a, b, carryin, sum, carryout);
+    a=0;b=1;carryin=1; #1000 
+    $display("%b  %b   %b  |  %b  %b", a, b, carryin, sum, carryout);
+    a=1;b=1;carryin=1; #1000 
+    $display("%b  %b   %b  |  %b  %b", a, b, carryin, sum, carryout);
     end
 endmodule
--- decoder.v
+++ decoder.v
@@ -1,5 +1,9 @@
 // Decoder circuit
 
+// Setting Delays
+`define AND and #50 //delay of 50 units
+`define NOT not #50
+
 module behavioralDecoder
 (
     output out0, out1, out2, out3,
@@ -17,6 +21,13 @@ module structuralDecoder
     input address0, address1,
     input enable
 );
-    // Your decoder code here
-endmodule
+    wire n_address0, n_address1;	// Inverted initial signal
+    `NOT address0inv(n_address0,address0); // Relevant function for n_address#
+    `NOT address1inv(n_address1,address1);
 
+    `AND andgate_out0(out0, n_address0, n_address1, enable);	// intermediate and gate
+    `AND andgate_out1(out1, address0, n_address1, enable);
+    `AND andgate_out2(out2, n_address0, address1, enable);
+    `AND andgate_out3(out3, address0, address1, enable);
+
+endmodule
