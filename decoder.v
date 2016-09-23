6d15065d8dbb78bdbc64fc75ebcffebbd6f1a6d8
Bonnie Ishiguro <bgishiguro@gmail.com>
2016-09-23 03:32:15 +0000

    Structural Decoder

--- decoder.v
+++ decoder.v
@@ -3,20 +3,35 @@
 module behavioralDecoder
 (
     output out0, out1, out2, out3,
-    input address0, address1,
+    input addr0, addr1,
     input enable
 );
     // Uses concatenation and shift operators
-    assign {out3,out2,out1,out0}=enable<<{address1,address0};
+    assign {out3,out2,out1,out0}=enable<<{addr1,addr0};
 endmodule
 
 
 module structuralDecoder
 (
     output out0, out1, out2, out3,
-    input address0, address1,
+    input addr0, addr1,
     input enable
 );
-    // Your decoder code here
+    wire nAddr0, nAddr1;
+
+    wire nAandnB, nAandB, AandnB, AandB;
+
+    not addr0Inv(nAddr0, addr0);
+    not addr1Inv(nAddr1, addr1);
+
+    and andgate(nAandnB, nAddr0, nAddr1);
+    and andgate(nAandB, nAddr0, addr1);
+    and andgate(AandnB, addr0, nAddr1);
+    and andgate(AandB, addr0, addr1);
+
+    and andgate(out0, enable, nAandnB);
+    and andgate(out1, enable, AandnB);
+    and andgate(out2, enable, nAandB);
+    and andgate(out3, enable, AandB);
 endmodule
 
