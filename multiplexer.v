8f97684f2e94855af9b92b02a9f584e71ade0d72
Bonnie Ishiguro <bgishiguro@gmail.com>
2016-09-23 00:46:26 +0000

    Multiplexer Test Bench

--- multiplexer.v
+++ multiplexer.v
@@ -3,12 +3,15 @@
 module behavioralMultiplexer
 (
     output out,
-    input address0, address1,
+    input addr0, addr1,
     input in0, in1, in2, in3
 );
     // Join single-bit inputs into a bus, use address as index
-    wire[3:0] inputs = {in3, in2, in1, in0};
-    wire[1:0] address = {address1, address0};
+     wire[3:0] inputs = {in3, in2, in1, in0};
+    
+    //wire[3:0] inputs = {in0, in1, in2, in3};
+
+    wire[1:0] address = {addr1, addr0};
     assign out = inputs[address];
 endmodule
 
