7499b91beb41edc258e6e17f759116b704aa5ed6
kghite <kghite@gmail.com>
2016-09-24 20:13:06 -0400

    Complete HW2

--- decoder.v
+++ decoder.v
@@ -1,5 +1,9 @@
 // Decoder circuit
 
+// Set time delays
+`define AND and #50
+`define NOT not #50
+
 module behavioralDecoder
 (
     output out0, out1, out2, out3,
@@ -17,6 +21,19 @@ module structuralDecoder
     input address0, address1,
     input enable
 );
-    // Your decoder code here
+    
+	wire na1;
+	wire na2;
+
+	// Invert inputs
+	`NOT a1inv(na1, address0);
+	`NOT a2inv(na2, address1);
+
+	// Outputs
+	`AND andgate1(out0, enable, na1, na2);
+	`AND andgate2(out1, enable, address0, na2);
+	`AND andgate3(out2, enable, address1, na1);
+	`AND andgate4(out3, enable, address0, address1);
+
 endmodule
 
--- multiplexer.t.v
+++ multiplexer.t.v
@@ -3,5 +3,162 @@
 `include "multiplexer.v"
 
 module testMultiplexer ();
-  // Your test code here
+
+	// Set up wires
+	reg in0, in1, in2, in3;
+	reg address0, address1;
+	wire out;
+
+	structuralMultiplexer multiplexer (out, address0, address1, in0, in1, in2, in3);
+
+	initial begin
+  
+		// Set dump file
+		$dumpfile("multiplexer_results.vcd");
+		$dumpvars;
+
+		// Address0 = 1, Address1 = 1
+		$display("A0 A1 | In0 In1 In2 In3 | Out0 | Expected Output");
+		address0=1; address1=1; in0=0; in1=0; in2=0; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=1; in0=0; in1=0; in2=0; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=1; in0=0; in1=0; in2=1; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=1; in0=0; in1=1; in2=0; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=1; in0=0; in1=1; in2=1; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=1; in0=0; in1=0; in2=1; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=1; in0=0; in1=1; in2=1; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=1; in0=0; in1=1; in2=0; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=1; in0=1; in1=0; in2=0; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=1; in0=1; in1=0; in2=0; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=1; in0=1; in1=0; in2=1; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=1; in0=1; in1=1; in2=0; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=1; in0=1; in1=1; in2=1; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=1; in0=1; in1=0; in2=1; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=1; in0=1; in1=1; in2=1; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=1; in0=1; in1=1; in2=0; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+
+		// Address0 = 1, Address1 = 0
+		$display("A0 A1 | In0 In1 In2 In3 | Out1 | Expected Output");
+		address0=1; address1=0; in0=0; in1=0; in2=0; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=0; in0=0; in1=0; in2=0; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=0; in0=0; in1=0; in2=1; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=0; in0=0; in1=1; in2=0; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=0; in0=0; in1=1; in2=1; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=0; in0=0; in1=0; in2=1; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=0; in0=0; in1=1; in2=1; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=0; in0=0; in1=1; in2=0; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=0; in0=1; in1=0; in2=0; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=0; in0=1; in1=0; in2=0; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=0; in0=1; in1=0; in2=1; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=0; in0=1; in1=1; in2=0; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=0; in0=1; in1=1; in2=1; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=0; in0=1; in1=0; in2=1; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=0; in0=1; in1=1; in2=1; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=1; address1=0; in0=1; in1=1; in2=0; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+
+
+		// Address0 = 0, Address1 = 1
+		$display("A0 A1 | In0 In1 In2 In3 | Out2 | Expected Output");
+		address0=0; address1=1; in0=0; in1=0; in2=0; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=1; in0=0; in1=0; in2=0; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=1; in0=0; in1=0; in2=1; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=1; in0=0; in1=1; in2=0; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=1; in0=0; in1=1; in2=1; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=1; in0=0; in1=0; in2=1; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=1; in0=0; in1=1; in2=1; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=1; in0=0; in1=1; in2=0; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=1; in0=1; in1=0; in2=0; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=1; in0=1; in1=0; in2=0; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=1; in0=1; in1=0; in2=1; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=1; in0=1; in1=1; in2=0; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=1; in0=1; in1=1; in2=1; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=1; in0=1; in1=0; in2=1; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=1; in0=1; in1=1; in2=1; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=1; in0=1; in1=1; in2=0; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+
+
+		// Address0 = 0, Address1 = 0
+		$display("A0 A1 | In0 In1 In2 In3 | Out3 | Expected Output");
+		address0=0; address1=0; in0=0; in1=0; in2=0; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=0; in0=0; in1=0; in2=0; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=0; in0=0; in1=0; in2=1; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=0; in0=0; in1=1; in2=0; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=0; in0=0; in1=1; in2=1; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=0; in0=0; in1=0; in2=1; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=0; in0=0; in1=1; in2=1; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=0; in0=0; in1=1; in2=0; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 0",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=0; in0=1; in1=0; in2=0; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=0; in0=1; in1=0; in2=0; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=0; in0=1; in1=0; in2=1; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=0; in0=1; in1=1; in2=0; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=0; in0=1; in1=1; in2=1; in3=1; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=0; in0=1; in1=0; in2=1; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=0; in0=1; in1=1; in2=1; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+		address0=0; address1=0; in0=1; in1=1; in2=0; in3=0; #1000
+		$display("%b  %b  | %b  %b  %b  %b  | %b | 1",address0, address1, in0, in1, in2, in3, out);
+
+	end
+
 endmodule
